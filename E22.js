4e27912db6f28a0e98f62480e336018eca1f06a5
Gaurav Pendharkar <gauravpendharkar2@gmail.com>
2022-05-17 22:06:28 +0530

    Add sum of array element

--- E22.js
+++ E22.js
@@ -1,9 +1,18 @@
-const pairArray = (arr) => {
-    arr.reduce((cur, nex) => {
-        console.log("---", cur[nex]);
-    }, {})
+{/*
+    Given array of integers. find the sum of its element.
 
+    For example, if the array arrayData = [1,2,3], 1+2+3=6, so return 6
+    
+    Function Description
+
+    Complete the simpleArraySum function in the editor below.
+    it must return the sum of the array elements as an integer.
+
+    simpleArraySum has the following parameter(s):
+*/}
+const simpleArraySum = (arr) => {
+    let result = arr.reduce((prev, cur) => prev + cur, 0);
+    console.log("Result", result);
 }
-
-const arrayData = [2, 2, 4, 3, 3];
-pairArray(arrayData);
+const arrayData = [2, 2, 4, 3];
+simpleArraySum(arrayData);
